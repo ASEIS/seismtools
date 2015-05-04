dcc19c9251d5fabcad8bad30023f95cbc5bc9f9e
Ricardo Taborda <ricardotaborda@gmail.com>
2015-05-03 23:00:33 -0500

    Completed record init and repr initial implementation.

--- seism.py
+++ seism.py
@@ -2,6 +2,7 @@ __author__ = 'rtaborda'
 
 import numpy as np
 
+
 class seism_record():
     """
     This class implements attributes and methods related to
@@ -12,33 +13,76 @@ class seism_record():
     of samples
     """
 
-    record_type = {'a': 'acceleration', 'v': 'velocity', 'd': 'displacement'}
-
-    def set_dt(self, delta_t):
-        if not isinstance(delta_t, float):
-            print "\nError with time-step (dt).\n"
-            return
-        self.dt = delta_t
+    record_type = {'a': 'Acceleration', 'v': 'Velocity', 'd': 'Displacement'}
 
     def set_samples(self, samples):
         if not isinstance(samples, int):
             print "\nError with samples type.\n"
-            return
         self.samples = samples
+    #end set_samples
+
+    def set_dt(self, delta_t):
+        if not isinstance(delta_t, float):
+            print "\nError with time-step (dt).\n"
+        self.dt = delta_t
+    #end set_dt
+
+    def set_data(self, sdata):
+        if not isinstance(sdata, np.array):
+            print "\nError with signal data: not a numpy array.\n"
+            return 3
+        self.sdata = sdata
+    #end set_data
 
     def set_type(self, stype):
         if not isinstance(stype, str) or stype not in self.record_type:
             print "\nError with signal type (must be: a, v, or d).\n"
-            return
         self.stype = stype
+    #end set_type
 
     def __init__(self, *args, **kwargs):
         """
         Initialize the record structure with all attributes empty.
+        If no parameters then all attributes set to None
+        Can be initialize with unlabeled and labeld arguments
+        Correct order for unlabeled arguments is: samples, dt, data, and signal type
+        Acceptable
         """
-        self.stype = str()
-        self.samples = int()
-        self.dt = float()
-        self.data = np.array([], float)
 
-    def set_samples
+        # Initialize to default values
+        self.samples = None
+        self.dt = None
+        self.sdata = None
+        self.stype = None
+
+        if len(args) < 2:
+            self.set_samples(args[0])
+        if len(args) < 3:
+            self.set_dt(args[1])
+        if len(args) < 4:
+            self.set_data(args[2])
+        if len(args) < 5:
+            self.set_type(args[3])
+            # all arguments were given in unlabled format
+            return
+
+        if len(kwargs) > 0:
+            if 'samples' in kwargs:
+                self.set_samples(kwargs['samples'])
+            if 'dt' in kwargs:
+                self.set_dt(kwargs['dt'])
+            if 'data' in kwargs:
+                self.set_data(kwargs['data'])
+            if 'type' in kwargs:
+                self.set_type(kwargs['type'])
+
+        return
+    #end __init__
+
+    def __repr__(self):
+        print self.record_type[self.stype]
+        print "Samples: "
+        print "Delta t: %f" % self.dt
+        print "Data:"
+        print self.sdata
+    #end __repr__

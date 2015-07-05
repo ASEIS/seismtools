Add function calculating response spectrum; add average slide in matrix; fix errors

compare_signals.py
@@ -45,13 +45,14 @@ def read_her_file(filename):
 	time = dis_ns = dis_ew = dis_up = vel_ns = vel_ew = vel_up = acc_ns = acc_ew = acc_up = np.array([],float)
 
 	try:
-		time, dis_ns, dis_ew, dis_up, vel_ns, vel_ew, vel_up, acc_ns, acc_ew, acc_up = np.loadtxt(filename, skiprows = 2, unpack = True)
+		time, dis_ns, dis_ew, dis_up, vel_ns, vel_ew, vel_up, acc_ns, acc_ew, acc_up = np.loadtxt(filename, comments='#', unpack = True)
+		print time
 	except IOError:
 		print "[ERROR]: error loading her file. "
 		return False  
 
 	samples = dis_ns.size 
-	dt = time[2] - time[1]
+	dt = time[1]
 
 	# samples, dt, data, acceleration, velocity, displacement 
 	psignal_ns = seism_psignal(samples, dt, np.c_[dis_ns, vel_ns, acc_ns], 'c', acc_ns, vel_ns, dis_ns)
@@ -68,8 +69,10 @@ def plot_signals(title, signal1, signal2):
 	"""
 	This function is to plot Signals with Fourier Amplitude Spectura. 
 	"""
-	global fmin
-	global fmax 
+	# global fmin
+	# global fmax 
+	fmin = 0.05
+	fmax = 4
 
 	if (not isinstance(signal1, seism_signal)) or (not isinstance(signal2, seism_signal)):
 		print "[ERROR]: Invalid instance type: can only plot signal objects."
@@ -87,8 +90,8 @@ def plot_signals(title, signal1, signal2):
 	t2 = np.arange(0, samples2*dt2, dt2)
 
 	points = get_points(samples1, samples2)
-	freq1, fas1 = FAS(signal1.data, dt1, points)
-	freq2, fas2 = FAS(signal2.data, dt2, points)
+	freq1, fas1 = FAS(signal1.data, dt1, points, fmin, fmax, 3)
+	freq2, fas2 = FAS(signal2.data, dt2, points, fmin, fmax, 3)
 
 	f, axarr = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 5))
 	axarr[0].set_title(title)
@@ -109,8 +112,11 @@ def plot_stations(station1, station2):
 	This function is to plot two lists of psignals with Fourier Amplitude Spectra. 
 	station = a list of 3 psignals for three orientation. 
 	"""
-	global fmin 
-	global fmax 
+	# global fmin 
+	# global fmax 
+	fmin = 0.05
+	fmax = 4
+
 	dtype = ['Displacement', 'Velocity', 'Acceleration']
 	orientation = ['N/S', 'E/W', 'Up/Down']
 
@@ -147,8 +153,8 @@ def plot_stations(station1, station2):
 			t2 = np.arange(0, samples2*dt2, dt2)
 
 			points = get_points(samples1, samples2)
-			freq1, fas1 = FAS(data1, dt1, points)
-			freq2, fas2 = FAS(data2, dt2, points)
+			freq1, fas1 = FAS(data1, dt1, points, fmin, fmax, 3)
+			freq2, fas2 = FAS(data2, dt2, points, fmin, fmax, 3)
 
 			axarr[j][0].set_title(title)
 			axarr[j][0].plot(t1,data1,'r',t2,data2,'b')

gof.py
@@ -8,6 +8,7 @@
 import sys
 import os
 import numpy as np
+import math 
 from seism import *
 from stools import *
 
@@ -138,6 +139,8 @@ def filt(psignal):
 
 def S(p1, p2):
 	# S(p1, p2) = 10*exp{-[(p1-p2)/min(p1, p2)]^2}
+	if p1 == p2:
+		return 10 
 	s = 10*np.exp(-((p1-p2)/min(p1, p2))**2)
 	return s 
 
@@ -154,7 +157,7 @@ def cal_peak(data1, data2):
 
 def I(data, dt):
 	# I(t) = max|integral(data^2)dt|
-	return np.amax(np.cumsum(data*data*dt))
+	return np.amax(np.cumsum(data*data)*dt)
 
 
 def cal_SI(signal1, signal2):
@@ -173,7 +176,7 @@ def cal_SI(signal1, signal2):
 
 def N(data, dt):
 	"""N = Ie(t)/IE = Ia(t)/IA"""
-	return np.cumsum(data*data*dt)/I(data, dt)
+	return np.cumsum(data*data)*dt/I(data, dt)
 
 def F(N1, N2):
 	return np.absolute(N1-N2)
@@ -191,14 +194,14 @@ def cal_SD(signal1, signal2):
 
 	return SDa, SDe
 
-def cal_Sfs(signal1, signal2):
+def cal_Sfs(signal1, signal2, fmin, fmax):
 	"""
 	calculate the score for Fourier Spectra
 	Sfs = mean(S(FS1, FS2))
 	"""
 	points = get_points(signal1.samples, signal2.samples)
-	fs1 = FAS(signal1.accel, signal1.dt, points)[-1]
-	fs2 = FAS(signal2.accel, signal2.dt, points)[-1]
+	fs1 = FAS(signal1.velo, signal1.dt, points, fmin, fmax, 3)[-1]
+	fs2 = FAS(signal2.velo, signal2.dt, points, fmin, fmax, 3)[-1]
 	s = np.array([],float)
 
 	for i in range(0, fs1.size):
@@ -214,12 +217,83 @@ def cal_C(a1, a2, dt):
 	C* = 10*max(C(a1, a2), 0)
 	C = integral(a1, a2)dt/((integral(a1^2)dt^1/2)*(integral(a2^2)dt^1/2))
 	"""
-	x = np.cumsum(a1*a2*dt)
-	y = np.cumsum(a1*a1*dt)
-	z = np.cumsum(a2*a2*dt)
+	x = np.cumsum(a1*a2)*dt
+	y = np.cumsum(a1*a1)*dt
+	z = np.cumsum(a2*a2)*dt
 	c = x/(np.power(y, 0.5)*np.power(z, 0.5))
 	cc = 10*np.amax(c, 0)
 	return cc 
+
+def get_period(fmin, fmax):
+	"""Return an array of period T"""
+	tmin = 1/fmax 
+	tmax = 1/fmin 
+	period = np.logspace(tmin, tmax, num=20)
+	return period 
+
+
+def max_osc_response(acc, dt, csi, period, ini_disp, ini_vel):
+	signal_size = acc.size 
+
+	# initialize numpy arrays
+	d = np.empty((signal_size))
+	v = np.empty((signal_size))
+	aa = np.empty((signal_size)) 
+
+	d[0] = ini_disp
+	v[0] = ini_vel
+
+	w = 2*math.pi/period
+	ww = w**2 
+	csicsi = csi**2 
+	dcsiw=2*csi*w
+
+	rcsi=math.sqrt(1-csicsi)
+	csircs=csi/rcsi
+	wd=w*rcsi
+	ueskdt=-1/(ww*dt)
+	dcsiew=2*csi/w
+	um2csi=(1-2*csicsi)/wd
+	e=math.exp(-w*dt*csi)
+	s=math.sin(wd*dt)
+	c0=math.cos(wd*dt);
+	aa[0]=-ww*d[0]-dcsiw*v[0]
+
+	ca=e*(csircs*s+c0)
+	cb=e*s/wd
+	cc=(e*((um2csi-csircs*dt)*s-(dcsiew+dt)*c0)+dcsiew)*ueskdt
+	cd=(e*(-um2csi*s+dcsiew*c0)+dt-dcsiew)*ueskdt
+	cap=-cb*ww
+	cbp=e*(c0-csircs*s)
+	ccp=(e*((w*dt/rcsi+csircs)*s+c0)-1)*ueskdt
+	cdp=(1-ca)*ueskdt
+
+	for i in range(1, signal_size):
+		d[i] = ca*d[i-1]+cb*v[i-1]+cc*acc[i-1]+cd*acc[i]
+		v[i] = cap*d[i-1]+cbp*v[i-1]+ccp*acc[i-1]+cdp*acc[i]
+		aa[i] = -ww*d[i]-dcsiw*v[i]
+
+	maxdisp = np.amax(np.absolute(d))
+	maxvel = np.amax(np.absolute(v))
+	maxacc = np.amax(np.absolute(aa))
+
+	return maxacc
+
+def cal_Ssa(SA1, SA2):
+	"""Calculate the score for Reponse Spectra"""
+	# SA = [maxdisp, maxvel, maxacc]
+	# print SA1
+	# print SA2
+
+	if len(SA1) != len(SA2): 
+		return 0 
+
+	ss = []
+	for i in range(0, len(SA1)):
+		ss.append(S(SA1[i], SA2[i]))
+
+	return np.mean(ss)
+
 
 
 
@@ -234,7 +308,7 @@ def scores_matrix(station1, station2):
 	# print bands 
 	c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = c10 = c11 = avg = 0.0
 
-	matrix = np.empty((3, len(bands)+1, 12))
+	matrix = np.empty((4, len(bands)+1, 12))
 
 	for i in range(0, len(station1)):
 		for j in range(0, len(bands)-1): 
@@ -249,10 +323,11 @@ def scores_matrix(station1, station2):
 			# print fmin, fmax 
 
 			# set the fmin and fmax being used in stools.py 
-			set_bound(fmin, fmax)
+			# set_bound(fmin, fmax)
 
 			signal1 = filt(station1[i])
 			signal2 = filt(station2[i])
+			period = get_period(fmin, fmax)
 
 			c1, c2 = cal_SD(signal1, signal2)
 			c3, c4 = cal_SI(signal1, signal2)
@@ -260,7 +335,15 @@ def scores_matrix(station1, station2):
 			c6 = cal_peak(signal1.velo, signal2.velo)
 			c7 = cal_peak(signal1.displ, signal2.displ)
 
-			c9 = cal_Sfs(signal1, signal2)
+			SA1 = []
+			SA2 = []
+			for p in period:
+				SA1.append(max_osc_response(signal1.accel, signal1.dt, 0.05, p, 0, 0))
+				SA2.append(max_osc_response(signal2.accel, signal2.dt, 0.05, p, 0, 0))
+
+			c8 = cal_Ssa(SA1, SA2)
+
+			c9 = cal_Sfs(signal1, signal2, fmin, fmax)
 			c10 = cal_C(signal1.accel, signal2.accel, signal1.dt)
 
 
@@ -277,25 +360,29 @@ def scores_matrix(station1, station2):
 		# FS2 = avg(b1-b6)
 		# FS1 = avg(bb-b6)
 		for j in range(0, 12):
-			avg2 = np.average(matrix[i][:,j][1:6])
+			avg2 = np.average(matrix[i][:,j][1:len(bands)])
 			fs2 = np.append(fs2, avg2)
 			fs2 = np.around(fs2, decimals=2)
 
-			avg1 = np.average(matrix[i][:,j][:6])
+			avg1 = np.average(matrix[i][:,j][:len(bands)])
 			fs1 = np.append(fs1, avg1)
 			fs1 = np.around(fs1, decimals=2)
 
 
+		# print fs1 
+		# print fs2
 		matrix[i][-1] = fs1
 		matrix[i][-2] = fs2
 
 
+	# adding the slide contain all average values 
+	for i in range(0, len(bands)+1):
+		for j in range(0, 12):
+			average = (matrix[0][i][j] + matrix[1][i][j] + matrix[2][i][j])/3
+			matrix[3][i][j] = round(average, 2)
 
-		# fs2 = np.append([], avg2)
-		# avg1 = np.average(matrix[i][:,0])
-		# fs1 = np.append([], avg1)
+		pass 
 
-		# matrix = np.append(matrix, [fs2, fs1])
 
 	
 	return matrix
@@ -335,7 +422,7 @@ def print_scores(matrix):
 # print_scores(matrix)
 
 
-for i in range(0, 3):
-	for j in range(0, len(bands)):
+for i in range(0, 4):
+	for j in range(0, len(bands)+1):
 		print matrix[i][j]
-	print "----------------------------------------------------------------------"
+	print "---------------------------------------------------------------------------------------"

sdc.py
@@ -8,6 +8,7 @@ from seism import *
 from stools import *
 
 destination = '' 
+header = ''
 def get_destination(d):
     """The function is to get the user input from process.py."""
     global destination
@@ -50,6 +51,7 @@ def load_file(filename):
 		print "[ERROR]: process Waveform files in ascii format only. "
 		return 
 
+	global header 
 	band = {'H': '80-250Hz', 'B': '10-80Hz', 'E': '80-250Hz'}
 	data_type = {'H': 'v', 'L': 'v', 'N': 'a'}
 	# v for velocity; a for acceleration, d for displacement 
@@ -57,20 +59,20 @@ def load_file(filename):
 	network = ''
 	station = ''
 	dt = 0.0
-	orientation = ''
+	# orientation = ''
 	date = ''
 	dtype = ''
 
 	f = filename.split('/')[-1]
 	tmp = f.split('.')
 	event = tmp[0]
-	network = tmp[1].upper()
-	station_id = tmp[2].upper()
-	info = tmp[3]
+	# network = tmp[1].upper()
+	# station_id = tmp[2].upper()
+	# info = tmp[3]
 
-	sample_rate = info[0].upper()
-	instr_type = info[1].upper()
-	orientation = info[2]
+	# sample_rate = info[0].upper()
+	# instr_type = info[1].upper()
+	# orientation = info[2]
 
 	try:
 		f = open(filename, 'r')
@@ -78,7 +80,7 @@ def load_file(filename):
 		print e
 		return 
 
-	data = np.loadtxt(filename, skiprows = 1, unpack = True)
+	data = np.loadtxt(filename, comments = '#', unpack = True)
 	samples = data.size -1 
 
 	for line in f:
@@ -109,7 +111,10 @@ def load_file(filename):
 		dtype = data_type[instr_type]
 
 	signal = seism_signal(samples, dt, data, dtype)
-	filename = event + '.' + network + '.' + station_id + '.' + sample_rate + instr_type
+	# print date 
+	header = "# " + network + " " + station + " " + "ASCII" + " " + date + " " + str(samples) + " " + str(dt) + "\n"
+	# print header 
+	# filename = event + '.' + network + '.' + station + '.' + sample_rate + instr_type
 	# record = seism_record(samples, dt, data, dtype, station, '', '', )
 	# samples, dt, data, signal type, station, 
  #        location_lati, location_longi, depth, date, time, orientation
@@ -216,7 +221,7 @@ def print_her(file_dict):
 
 
     # TODO: get time and date from read_event()
-    header = "# " + network + " " + station + " " + "ASCII" + " " + "date" + "," + "time" + " " + str(signal.samples) + " " + str(signal.dt) + "\n"
+    # header = "# " + network + " " + station + " " + "ASCII" + " " + "date" + "," + "time" + " " + str(signal.samples) + " " + str(signal.dt) + "\n"
     f.write(header)
 
     descriptor = '{:>12}' + '  {:>12}'*9 + '\n'

stools.py
@@ -65,24 +65,34 @@ def ellip_filter(data, dt, *args, **kwargs):
     data = filtfilt(b, a, data)
     return data 
 
+def smooth(data, factor): 
+    # factor = 3; c = 0.5, 0.25, 0.25
+    # TODO: fix coefficients for factors other than 3 
+    c = 0.5/(factor-1)
+    for i in range(1, data.size-1):
+        data[i] = 0.5*data[i] + c*data[i-1] + c*data[i+1]
+    return data 
 
-def FAS(data, dt, points):
-	global fmin 
-	global fmax 
-	afs = abs(np.fft.fft(data, points))*dt 
-	# freq = (1/signal.dt)*range(points)/points 
-	freq = (1/dt)*np.array(range(points))/points 
 
-	deltaf = (1/dt)/points
+def FAS(data, dt, points, fmin, fmax, s_factor):
+    afs = abs(np.fft.fft(data, points))*dt
+	# freq = (1/signal.dt)*range(points)/points
+    freq = (1/dt)*np.array(range(points))/points
 
-	inif = int(fmin/deltaf)
-	endf = int(fmax/deltaf) + 1
+    deltaf = (1/dt)/points
 
-	afs = afs[inif:endf]
-	freq = freq[inif:endf]
+    inif = int(fmin/deltaf)
+    endf = int(fmax/deltaf) + 1
+    
+    afs = afs[inif:endf]
+    afs = smooth(afs, s_factor)
+    freq = freq[inif:endf]
+    return freq, afs
 
-	# freq = t; data = afs 
-	return freq, afs 
+    # afs = afs[inif:endf]
+    # afs = smooth(afs, s_factor) # smooth the curve 
+    # freq = freq[inif:endf]
+    # return freq, afs 
 
 def get_points(samples1, samples2):
 	# points is the least base-2 number that is greater than max samples 
@@ -97,6 +107,6 @@ def set_bound(f1, f2):
 	fmin = f1 
 	fmax = f2
 
-	# print fmin, fmax 
+	print fmin, fmax 
 # end of set_bound
 

test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
+import math 
 # z = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], float)
 # y = np.array([z, z, z, z, z, z, z, z], float)
 # x = np.array([y, y, y], float)
@@ -8,7 +9,17 @@ import numpy as np
 
 # print x.shape
 
-data = range(1,5)
-print data 
-print np.average(data[:2])
-print np.average(data)
+# data = range(1,5)
+# print data 
+# print np.average(data[:2])
+# print np.average(data)
+
+
+
+period = np.logspace(1/0.05, 1/4, num=20)
+for p in period:
+	print p 
+# print period
+
+# w = 2*math.pi*period
+# print w 
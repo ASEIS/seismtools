81596918678ec6b53c9345550d5bf306930c87a7
Keli Cheng <ikcheng322@gmail.com>
2015-07-19 15:36:37 -0500

    add time axis limits; add cutting option

--- compare_signals.py
+++ compare_signals.py
@@ -11,20 +11,28 @@ from matplotlib import gridspec
 from seism import *
 from stools import *
 
-def set_axis():
-	"""setting the bound for ploting"""
-	xfmin = raw_input('== Enter fmin for ploting: ')
-	xfmax = raw_input('== Enter fmax for ploting: ')
+def set_axis(xtype):
+	"""setting bounds for ploting"""
+	msg = ['', '']
+
+	if xtype == 'freq':
+		msg = ['F-min', 'F-max']
+	elif xtype == 'time':
+		msg = ['T-min', 'T-max']
+
+	xfmin = raw_input('== Enter ' + msg[0] + ' for ploting: ')
+	xfmax = raw_input('== Enter ' + msg[1] + ' for ploting: ')
+
 	try: 
 		xfmin = float(xfmin)
 		xfmax = float(xfmax)
 	except ValueError:
 		print "[ERROR]: invalid input type: floats or integers ONLY."
-		return set_bound()
+		return set_axis(xtype)
 
 	while xfmin >= xfmax:
-		print "[ERROR]: fmax must be greater than fmin."
-		return set_bound()
+		print "[ERROR]: max must be greater than min."
+		return set_axis(xtype)
 	if xfmin < 0.1: 
 		xfmin = 0
 
@@ -46,16 +54,26 @@ def set_bound():
 
 	return fmin, fmax 
 
-def set_filter():
-	f_flag = ''
-	while f_flag not in ['Y', 'N']: 
-		f_flag = raw_input('== Do you want to filter the data (y/n): ')[0].upper()
+def set_flag(ftype):
+	"""get user's choice on cutting signal/filter data"""
+	question = ''
+	if ftype == 'filter':
+		question = '== Do you want to filter the data (y/n): '
+	elif ftype == 'cut':
+		question = '== Do you want to cut the signal (y/n): '
+
+	flag = ''
+	while flag not in ['Y', 'N']: 
+		flag = raw_input(question)
+		if not flag:
+			return set_flag(ftype)
+		flag = flag[0].upper()
 	
-	if f_flag == 'Y':
-		f_flag = True 
+	if flag == 'Y':
+		flag = True 
 	else:
-		f_flag = False 
-	return f_flag
+		flag = False 
+	return flag
 
 # ---------------------------------------------------------------------------------------------
 
@@ -115,19 +133,28 @@ def read_her(filename):
 
 
 
-def plot_signals(title, signal1, signal2):
+def plot_signals(filenames, signal1, signal2):
 	"""
 	This function is to plot Signals with Fourier Amplitude Spectura. 
 	"""
-	xfmin, xfmax = set_axis()
+	xtmin, xtmax = set_axis('time')
+	# t_flag = cut()
+
+	xfmin, xfmax = set_axis('freq')
 	fmin, fmax = set_bound()
-	flag = set_filter()
+	f_flag = set_flag('filter')
+	c_flag = set_flag('cut')
 
 	if (not isinstance(signal1, seism_signal)) or (not isinstance(signal2, seism_signal)):
 		print "[ERROR]: Invalid instance type: can only plot signal objects."
 		return 
 
 	plt.close('all')
+
+	file1 = filenames[0]
+	file2 = filenames[1]
+
+	title = 'Acceleration ONLY: '
 
 	samples1 = signal1.samples
 	samples2 = signal2.samples
@@ -139,26 +166,42 @@ def plot_signals(title, signal1, signal2):
 	t2 = np.arange(0, samples2*dt2, dt2)
 
 	# filtering data
-	if flag:
+	if f_flag:
 		data1 = bandpass_filter(data1, dt1, fmin, fmax)
 		data2 = bandpass_filter(data2, dt2, fmin, fmax)
 
+	# cutting signals by bounds
+	min_i = int(xtmin/dt1) 
+	max_i = int(xtmax/dt1)
+	c_data1 = data1[min_i:max_i]
+
+	min_i = int(xtmin/dt2) 
+	max_i = int(xtmax/dt2)
+	c_data2 = data2[min_i:max_i]
+
+	t1 = np.arange(xtmin, xtmax, dt1)
+	t2 = np.arange(xtmin, xtmax, dt2)
+
 	points = get_points(samples1, samples2)
-	freq1, fas1 = FAS(signal1.data, dt1, points, fmin, fmax, 3)
-	freq2, fas2 = FAS(signal2.data, dt2, points, fmin, fmax, 3)
+	if not c_flag:
+		# if user chooses not to cut; use origina/filted data for FAS 
+		freq1, fas1 = FAS(data1, dt1, points, fmin, fmax, 3)
+		freq2, fas2 = FAS(data2, dt2, points, fmin, fmax, 3)
+
+	else: 
+		# else uses cutted data for FAS
+		freq1, fas1 = FAS(c_data1, dt1, points, fmin, fmax, 3)
+		freq2, fas2 = FAS(c_data2, dt2, points, fmin, fmax, 3)
 
 	# gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1]) 
 	f, axarr = plt.subplots(nrows = 1, ncols = 2, figsize = (9, 3))
 
-	# axarr[0].set_title(title)
-	# axarr[0].plot(t1,data1,'r',t2,data2,'b')
-
-	# axarr[1].set_title('Fourier Amplitude Spectra')
-	# axarr[1].plot(freq1,fas1,'r',freq2,fas2,'b')
-
 	axarr[0] = plt.subplot2grid((1, 3), (0, 0), colspan=2)
 	axarr[0].set_title(title)
-	axarr[0].plot(t1,data1,'r',t2,data2,'b')
+	axarr[0].plot(t1,c_data1,'r',t2,c_data2,'b')
+
+	plt.legend([file1, file2])
+	plt.xlim(xtmin, xtmax)
 
 	axarr[1] = plt.subplot2grid((1, 3), (0, 2))
 	axarr[1].set_title('Fourier Amplitude Spectra') 
@@ -172,14 +215,16 @@ def plot_signals(title, signal1, signal2):
 
 
 
-def plot_stations(station1, station2):
+def plot_stations(filenames, station1, station2):
 	"""
 	This function is to plot two lists of psignals with Fourier Amplitude Spectra. 
 	station = a list of 3 psignals for three orientation. 
 	"""
-	xfmin, xfmax = set_axis()
+	xtmin, xtmax = set_axis('time')
+	xfmin, xfmax = set_axis('freq')
 	fmin, fmax = set_bound()
-	flag = set_filter()
+	f_flag = set_flag('filter')
+	c_flag = set_flag('cut')
 
 	dtype = ['Displacement', 'Velocity', 'Acceleration']
 	orientation = ['N/S', 'E/W', 'Up/Down']
@@ -188,6 +233,8 @@ def plot_stations(station1, station2):
 		print "[ERROR]: plot_stations only handles stations with 3 channels."
 		return 
 
+	file1 = filenames[0]
+	file2 = filenames[1]
 
 	# from displacement to velocity to acceleration
 	for i in range(0, 3):
@@ -215,17 +262,34 @@ def plot_stations(station1, station2):
 			dt2 = signal2.dt 
 
 			# filtering data
-			if flag:
+			if f_flag:
 				data1 = bandpass_filter(data1, dt1, fmin, fmax)
 				data2 = bandpass_filter(data2, dt2, fmin, fmax)
 
+			# cutting signal by bounds
+			min_i = int(xtmin/dt1) 
+			max_i = int(xtmax/dt1)
+			c_data1 = data1[min_i:max_i]
 
-			t1 = np.arange(0, samples1*dt1, dt1)
-			t2 = np.arange(0, samples2*dt2, dt2)
+			min_i = int(xtmin/dt2) 
+			max_i = int(xtmax/dt2)
+			c_data2 = data2[min_i:max_i]
+
+			t1 = np.arange(xtmin, xtmax, dt1)
+			t2 = np.arange(xtmin, xtmax, dt2)
 
 			points = get_points(samples1, samples2)
-			freq1, fas1 = FAS(data1, dt1, points, fmin, fmax, 3)
-			freq2, fas2 = FAS(data2, dt2, points, fmin, fmax, 3)
+			if not c_flag:
+				# if user chooses not to cut; use original data to calculate FAS
+				freq1, fas1 = FAS(data1, dt1, points, fmin, fmax, 3)
+				freq2, fas2 = FAS(data2, dt2, points, fmin, fmax, 3)
+			else: 
+				# use cutted signals to calculate FAS
+				freq1, fas1 = FAS(c_data1, dt1, points, fmin, fmax, 3)
+				freq2, fas2 = FAS(c_data2, dt2, points, fmin, fmax, 3)
+
+			# 	t1 = np.arange(0, samples1*dt1, dt1)
+			# 	t2 = np.arange(0, samples2*dt2, dt2)
 
 			# axarr[j][0].set_title(title)
 			# axarr[j][0].plot(t1,data1,'r',t2,data2,'b')
@@ -235,7 +299,10 @@ def plot_stations(station1, station2):
 
 			axarr[j][0] = plt.subplot2grid((3, 3), (j, 0), colspan=2, rowspan=1)
 			axarr[j][0].set_title(title)
-			axarr[j][0].plot(t1,data1,'r',t2,data2,'b')
+			axarr[j][0].plot(t1,c_data1,'r',t2,c_data2,'b')
+
+			plt.legend([file1, file2])
+			plt.xlim(xtmin, xtmax)
 
 			axarr[j][1] = plt.subplot2grid((3, 3), (j, 2), rowspan=1)
 			axarr[j][1].set_title('Fourier Amplitude Spectra') 
@@ -262,40 +329,25 @@ def test(psignal):
 
 
 def compare_txt(file1, file2):
-	# revert the order of files to V1, V2
-	if 'V1' not in file1.split('.')[-2]: 
-		tmp = file1
-		file1 = file2 
-		file2 = tmp 
-
-	# print file1, file2
-
 	signal1 = read_txt(file1)
 	signal2 = read_txt(file2)
 	if (not isinstance(signal1, seism_signal)) or (not isinstance(signal2, seism_signal)):
 		print "[ERROR]: Invalid instance type: can only compare signal objects."
 		return 
 
-	title = 'Acceleration ONLY: \n' + file1 + ' ' + file2
-	plot_signals(title, signal1, signal2)
+	filenames = [file1, file2]
+	plot_signals(filenames, signal1, signal2)
 # end of compare_txt
 
 
 
 def compare_her(file1, file2):
-	# revert the order of files to V1, V2
-	if 'V1' not in file1.split('.')[-2]: 
-		tmp = file1
-		file1 = file2 
-		file2 = tmp 
-
-	# print file1, file2
-
 	# station = [psignal_ns, psignal_ew, psignal_up]
 	station1 = read_her(file1) 
 	station2 = read_her(file2)
 
-	plot_stations(station1, station2)
+	filenames = [file1, file2]
+	plot_stations(filenames, station1, station2)
 
 	# test(station1[0])
 # end of compare_her

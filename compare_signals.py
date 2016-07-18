41a72861e7165047f3cc0c6987297f4fb0345d71
Fabio Silva <fsilva@usc.edu>
2016-07-18 15:15:17 -0700

    Added minor grid lines to FAS plots.

--- compare_signals.py
+++ compare_signals.py
@@ -591,7 +591,7 @@ def simple_plot(parameter, filenames, stations):
 
         axarr[i][1] = plt.subplot2grid((3, 4), (i, 2), rowspan=1, colspan=1)
         axarr[i][1].set_title('Fourier Amplitude Spectra')
-        axarr[i][1].grid(True)
+        axarr[i][1].grid(True, which='both')
         axarr[i][1].set_xscale('log')
         axarr[i][1].set_yscale('log')
         for freq, fas, style in zip(freqs, fas_s, styles):
